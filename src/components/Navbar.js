5a1fe97244fc6ab21cb2638ea2cc1e92494db86c
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-23 21:43:56 +0900

    Add react icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
-
+import { FiAlignJustify } from "react-icons/fi";
 const Navbar = () => {
   return (
     <div>
       <nav>
+        <FiAlignJustify />
         <ul>
           <li>
             <Link to="/">Home</Link>
