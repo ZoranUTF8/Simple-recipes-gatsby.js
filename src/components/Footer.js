72ec5039c1b93537381da75d86d608367d620b46
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-23 21:19:33 +0900

    Add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-          <h1>FOOTER</h1>  
-        </div>
-    )
-}
+  return (
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>Jednostavni recepti</span>{" "}
+        Napravljeno sa <a href="https://www.gatsbyjs.com/">Gatsby</a>
+        <br />
+        Zoran Janjic
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
