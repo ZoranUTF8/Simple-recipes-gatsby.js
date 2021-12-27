34272b9491295f641fe1cc30e9774d7b0ec3d068
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-27 13:33:18 +0900

    Add tags page

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,11 +1,44 @@
 import React from "react";
 import Layout from "../components/Layout";
-const Tags = () => {
+import TagList from "../components/TagList";
+import { graphql, Link } from "gatsby";
+import tagsSetUp from "../utils/tagsSetUp";
+
+const Tags = ({
+  data: {
+    allContentfulRecipe: { nodes: recipes },
+  },
+}) => {
+  const tags = tagsSetUp(recipes);
   return (
     <Layout>
-      <h1>Tags page</h1>
+      <main className="page">
+        <section className="tags-page">
+          {tags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              <Link key={index} to={`/${text}`} className="tag">
+                <h5>{text}</h5>
+                <p>{value} recept.</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
-  )
-}
+  );
+};
 
-export default Tags
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
+
+export default Tags;
