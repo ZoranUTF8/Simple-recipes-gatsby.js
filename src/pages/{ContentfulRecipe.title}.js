4f1520ca2f7278a723f3e4f73e4dfc05d77cf4e6
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-27 13:51:55 +0900

    Add dinamic recipes page

---
+++ src/pages/{ContentfulRecipe.title}.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { graphql } from "gatsby";
+
+const RecipeTemplate = (props) => {
+  return (
+    <div>
+      <h2> {props.params.title}</h2>
+    </div>
+  );
+};
+
+export const query = graphql`
+  {
+  }
+`;
+
+export default RecipeTemplate;
