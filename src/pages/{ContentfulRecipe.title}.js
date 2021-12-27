a969ed17b787c89bfc4b0c9a40b1d87aafc05af9
Zoran <66476222+ZoranUTF8@users.noreply.github.com>
2021-12-27 17:43:12 +0900

    Add graphql to get single recipe

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,14 +1,53 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { BsClockHistory, BsClock, BsPeople } from "react-icons";
 
-const RecipeTemplate = (props) => {
+const RecipeTemplate = ({ data }) => {
+  const {
+    title,
+    cookTime,
+    content: { tags, instructions, ingredients, tools },
+    prepTime,
+    servings,
+    description: { description },
+    image,
+  } = data.contentfulRecipe;
+
+  const pathToImage = getImage(image);
+
   return (
     <div>
-      <h2> {props.params.title}</h2>
+      <main className="page">
+          <div className="recipe-page">
+              <h2>{title}</h2>
+          </div>
+      </main>
     </div>
   );
 };
 
-
+export const query = graphql`
+  query getSingleRecipe($title: String) {
+    contentfulRecipe(title: { eq: $title }) {
+      title
+      cookTime
+      content {
+        instructions
+        ingredients
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
+    }
+  }
+`;
 
 export default RecipeTemplate;
